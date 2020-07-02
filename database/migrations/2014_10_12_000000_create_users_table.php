1915d394eacfd5998b274daa9a911b324c933e07
Syahrul Safarila <sysafarila.official@gmail.com>
2020-07-02 16:05:23 +0700

    Creating Migration & Model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('nik')->unique();
+            $table->string('phone');
+            $table->enum('level', ['admin', 'petugas', 'masyarakat']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
